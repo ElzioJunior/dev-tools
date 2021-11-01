1696d0614e60b8cba458c6d337c154397108b146
Élzio Júnior da Silva <elzioj.s@gmail.com>
2021-11-01 11:18:52 -0300

    Update Runtime-Testing-Components.java

--- Runtime-Testing-Components.java
+++ Runtime-Testing-Components.java
@@ -1,4 +1,4 @@
-package br.com.***REMOVED***.cardsrate;
+package foobar;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
